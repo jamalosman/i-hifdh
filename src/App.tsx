e6157a3d057370fe272260403bf307dec8c83d76
Jamal Osman <jamal.osman93@gmail.com>
2021-03-05 17:33:00 +0000

    add surah list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SurahList from './surah-list/SurahList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>iHifdh</h1>
       </header>
+      <SurahList></SurahList>
     </div>
   );
 }
---
+++ src/surah-list/SurahList.tsx
@@ -0,0 +1,20 @@
+import surahs from '../resources/quran-uthmani.json';
+import './SurahList.css';
+
+function SurahList() {
+  let surahElements = surahs.map(x => 
+    <li className="surah-list-item">
+      <a className="surah-link" href={`/surahs/${x.index}`}>{x.index}. {x.name}</a>
+    </li>);
+
+  return (
+    <div className="surah-list-component">
+        <h2 className="surah-list-title">Surahs</h2>
+        <ul className="surah-list">
+          {surahElements}
+        </ul>
+    </div>
+  );
+}
+
+export default SurahList;
