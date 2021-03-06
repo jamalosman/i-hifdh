098d9242ae08e6ee9b9ae850254e2d4b3be46838
Ebrima Conta <ebrimaconta@gmail.com>
2021-03-06 20:48:34 +0000

    If not in home page show back to homepage

--- src/domain/Home/Home.tsx
+++ src/domain/Home/Home.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
 import SurahList from '../../components/SurahList/SurahList';
-
-function Home() {
+import { RouteComponentProps } from 'react-router-dom';
+type IHome = {
+  id: string;
+};
+const Home = ({ match }: RouteComponentProps<IHome>) => {
+  let id = match.params.id;
+  console.log(match.params);
   return (
     <div className='App'>
-      <Header />
+      <Header url={id} />
       <SurahList />
     </div>
   );
-}
+};
 
 export default Home;
